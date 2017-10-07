8e38ba78d4ae3f3cefeff3fe8fc453bc06bbb79c
nbg <nicholasnbg@gmail.com>
2017-10-07 21:56:24 +1100

    added more

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    console.log('app running');
+    $('.addNote').click(function(){
+        var newNote = $('#noteText').val();
+        var noteHTML = '<li>'+newNote+'</li>';
+        if(newNote.length>0){
+            $('.orderNotesList').append(noteHTML);
+            $('#noteText').val('');
+        };
+    });
 });
